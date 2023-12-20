4db80a7e5a4eb4362949e9fba04cce7329a65b22
Agência B-Web <agenciabweb@gmail.com>
2023-12-20 19:08:25 -0300

    Listando os usuarios do banco de dados

--- app/Models/UsuarioModel.php
+++ app/Models/UsuarioModel.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class UsuarioModel extends Model
-{
-    protected $table            = 'usuarios';
-    protected $returnType       = 'object';
-    protected $useSoftDeletes   = true;
-    protected $allowedFields    = ['nome', 'email', 'telefone'];
+class UsuarioModel extends Model {
+
+    protected $table = 'usuarios';
+    protected $returnType = 'object';
+    protected $useSoftDeletes = true;
+    protected $allowedFields = ['nome', 'email', 'telefone'];
+    protected $useTimestamps = true;
+    protected $createdField = 'criado_em';
+    protected $updatedField = 'atualizado_em';
+    protected $deletedField = 'deletado_em';
 }
